8a283b3417a7e2c4d389824a6b4a8d9be11a584a
Aruran Kirupanantha <arukirupa@gmail.com>
2024-04-06 12:06:55 +0530

    Authentication services (Register user, Login, Logout, forgot Passswor, change password, ,change userDetails

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const errorMiddleware = require("./middlewares/error");
+const auth = require("./routes/auth");
+
+const cookieParser = require("cookie-parser");
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5000",
+    credentials: true,
+  })
+);
+
+// app.use("/api", products);
+app.use("/api", auth);
+// app.use("/api", chat);
+// app.use("/api", cart);
+
+app.use(errorMiddleware);
+app.set("trust proxy", 1);
+
+module.exports = app;
